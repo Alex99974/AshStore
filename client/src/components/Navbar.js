93cd11e75df4fdb07a4659e3173fca83b2f4d71b
Firsov974 <tabacco974@gmail.com>
2022-09-25 21:12:13 +0300

    GraphQL work commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,84 +1,28 @@
-import React from 'react';
-import { Box, Text, Heading, Image, Button } from 'gestalt';
-import { NavLink, withRouter } from 'react-router-dom';
-import { getToken, clearToken, clearCart } from '../utils';
+import React from "react";
+import { Box, Text, Heading, Image } from "gestalt";
+import { NavLink } from "react-router-dom";
 
-// withRouter gives access to `history` in this instance (not always needed)
-export const Navbar = withRouter(props => {
-  const handleSignout = () => {
-    clearToken();
-    clearCart();
-    props.history.push('/');
-  };
-
-  return getToken() !== null ? (
-    <AuthNav handleSignout={handleSignout} />
-  ) : (
-    <UnAuthNav />
-  );
-});
-
-const AuthNav = ({ handleSignout }) => (
+const Navbar = () => (
   <Box
     display="flex"
     alignItems="center"
     justifyContent="around"
-    height={78}
+    height={70}
     color="midnight"
     padding={1}
-    shape={'roundedBottom'}
+    shape="roundedBottom"
   >
-    <NavLink to="/checkout">
-      <Text size="xl" color="white">
-        Checkout
-      </Text>
-    </NavLink>
-
-    <NavLink to="/">
-      <Box display="flex" alignItems="center">
-        <Box margin={2} heigh={50} width={50}>
-          <Image
-            alt="BrewHaha Logo"
-            naturalHeight={1}
-            naturalWidth={1}
-            src="./icons/logo.svg"
-          />
-        </Box>
-        <Heading size="xs" color="orange">
-          BrewHaha
-        </Heading>
-      </Box>
-    </NavLink>
-
-    <Button
-      color="transparent"
-      text="Sign out"
-      inline
-      size="md"
-      onClick={handleSignout}
-    />
-  </Box>
-);
-
-const UnAuthNav = () => (
-  <Box
-    display="flex"
-    alignItems="center"
-    justifyContent="around"
-    height={78}
-    color="midnight"
-    padding={1}
-    shape={'roundedBottom'}
-  >
+    {/* Sign In Link */}
     <NavLink to="/signin">
       <Text size="xl" color="white">
-        Sign in
+        Sign In
       </Text>
     </NavLink>
 
+    {/* Title and Logo */}
     <NavLink to="/">
       <Box display="flex" alignItems="center">
-        <Box margin={2} heigh={50} width={50}>
+        <Box margin={2} height={50} width={50}>
           <Image
             alt="BrewHaha Logo"
             naturalHeight={1}
@@ -92,10 +36,13 @@ const UnAuthNav = () => (
       </Box>
     </NavLink>
 
+    {/* Sign Up Link */}
     <NavLink to="/signup">
       <Text size="xl" color="white">
-        Sign up
+        Sign Up
       </Text>
     </NavLink>
   </Box>
 );
+
+export default Navbar;
