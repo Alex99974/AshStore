2dd3fb533769bd954ac1672bd96db4572a7c3132
Firsov974 <tabacco974@gmail.com>
2022-09-25 03:09:11 +0300

    Second commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,48 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
-import 'gestalt/dist/gestalt.css';
-import { getToken } from './utils';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
 
-import { App, Signin, Signup, Checkout, Navbar, Brews } from './components';
-
-import registerServiceWorker from './registerServiceWorker';
-
-// Route only accessible to authenticated users (e.g. checkout)
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      getToken() !== null ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{ pathname: '/signin', state: { from: props.location } }}
-        />
-      )
-    }
-  />
-);
-
-const Root = () => (
-  <Router>
-    <>
-      <Navbar />
-      <Switch>
-        <Route component={App} exact path="/" />
-        <Route component={Signin} path="/signin" />
-        <Route component={Signup} path="/signup" />
-        <PrivateRoute component={Checkout} path="/checkout" />
-        <Route component={Brews} path="/:brandId" />
-      </Switch>
-    </>
-  </Router>
-);
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
